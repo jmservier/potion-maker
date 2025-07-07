1443d93a120260117672768b3202c367f0b60ae6
yoann <yoann.r.dev@gmail.com>
2025-07-07 22:12:39 +0200

    feat: sort ingredients by name 1st render

--- src/queries/ingredients/getAllIngredients.ts
+++ src/queries/ingredients/getAllIngredients.ts
@@ -3,9 +3,14 @@ import prisma from "@/lib/prisma";
 
 export async function getAllIngredients(): Promise<Ingredient[]> {
   try {
-    const ingredients = await prisma.ingredient.findMany();
+    const ingredients = await prisma.ingredient.findMany({
+      orderBy: {
+        name: "asc",
+      },
+    });
     return ingredients;
   } catch (error) {
+    console.error("Failed to fetch ingredients", error);
     throw new Error("Failed to fetch ingredients");
   }
 }
