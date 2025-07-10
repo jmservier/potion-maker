fd8f7d4dfe38e81828b1fe4db74ac98476a75a76
yoann <yoann.r.dev@gmail.com>
2025-07-10 11:30:35 +0200

    feat: dynamically load ReactQueryDevtools for development environment

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,15 +1,23 @@
 "use client";
 
 import React from "react";
+import dynamic from "next/dynamic";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const ReactQueryDevtools = dynamic(
+  () =>
+    import("@tanstack/react-query-devtools").then(
+      (mod) => mod.ReactQueryDevtools,
+    ),
+  { ssr: false },
+);
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = React.useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools />
+      {process.env.NODE_ENV === "development" && <ReactQueryDevtools />}
     </QueryClientProvider>
   );
 }
